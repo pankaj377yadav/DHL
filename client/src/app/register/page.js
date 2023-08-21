8b72727fca40340bfda4cf16968ce52d45ca59ac
Pankaj Kumar Yadav <pankaj377yadav@gmail.com>
2023-08-21 08:52:00 +0545

    using chakra UI in Home page

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,21 +1,70 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
- 
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    passWord: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+  .email('Invalid email')
+  .required('Required'),
+});
 
-function page() {
+const ValidationSchemaExample = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        passWord: "",
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="firstName"
+          placeholder="Full Name"/>
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName"
+          placeholder="Last Name" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="passWord"
+          placeholder="PassWord"
+          type="password"/>
+          {errors.passWord && touched.passWord ? (
+            <div>{errors.passWord}</div>
+          ) : null}
+          <Field name="email"
+          placeholder="Email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
-    return (
 
-        <div>
 
-            Register page
-
-        </div>
-
-    )
-
-}
-
- 
-
-export default page
+export default ValidationSchemaExample
